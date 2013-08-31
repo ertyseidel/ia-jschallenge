8ee0aa3817f6956b3dc4cc83f1bf3c828a52e552
Erty Seidel <ertysdl+github@gmail.com>
2013-08-31 16:49:49 -0600

    Moved image code into IAGame function
    Moved image code into IAGame function, added height and width to canvas.
    Since there are only a certain set of images, I'm not making this
    particularly extensible (outside loading of image data) and just
    hard-coding the image URLs into the game, to save the trouble for such a
    small app.

--- canvas.js
+++ canvas.js
@@ -2,19 +2,38 @@
 	var IAGame = function (canvasElement, images){
 		if(!canvasElement.getContext) throw "canvasElement could not get context";
 		var ctx = canvasElement.getContext("2d"); //get canvas context element
-		var imagesLoaded = 0; //when this hits 5, all of the images are loaded
+		this.imagesLoaded = 0; //number of images which have finished loading
+
+		this.images = {
+			background: "ia-logo-back.png",
+			black: "ia-logo-dot-black.png",
+			blue: "ia-logo-dot-blue.png",
+			green: "ia-logo-dot-green.png",
+			red: "ia-logo-dot-red.png"
+		}
+
+		for (var key in this.images){
+			var src = this.images[key];
+			this.images[key] = new Image();
+			this.images[key].onload = function(){
+				this.imagesLoaded ++;
+			}
+			this.images[key].src = "./img/" + src;
+		};
+
 		/*
          * Update function which runs to update positions and check for winning
 		 */
 		this.update = function(){
-			if(imagesLoaded < 5) return;
+			if(this.imagesLoaded < Object.keys(this.images).length) return;
 
 		}
 		/*
 		 * Draw function which repaints canvas at (hopefully) 60 frames per second
 		 */
 		this.draw = function(){
-			
+			if(this.imagesLoaded < Object.keys(this.images).length) return;
+			ctx.drawImage(this.images.background, 0, 0);
 		}
 
 	};
@@ -23,28 +42,10 @@
 })(window);
 
 window.onload = function(){
-	var iagame = new IAGame(document.getElementById("app"));
-
-	var images = {
-		"background": new Image(),
-		"black": new Image(),
-		"blue": new Image(),
-		"green": new Image(),
-		"red": new Image()
-	}
-
-	var imgpath = "./img/"
-	for (var key in images){
-		images[key].onload = function(){
-			iagame.imagesLoaded ++;
-		}
-	};
-
-	images.background.src = imgpath + "ia-logo-back.png";
-	images.black.src = imgpath + "ia-logo-dot-black.png";
-	images.blue.src = imgpath + "ia-logo-dot-blue.png";
-	images.green.src = imgpath + "ia-logo-dot-green.png";
-	images.red.src = imgpath + "ia-logo-dot-red.png";
+	var canvas = document.getElementById("app");
+	canvas.style.width = "800px";
+	canvas.style.height = "600px";
+	var iagame = new IAGame(canvas);
 
 	var updateGame = function(){
 		window.requestAnimationFrame(updateGame);
