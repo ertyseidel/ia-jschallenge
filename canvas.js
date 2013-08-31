2104e99bd851777a0824674c5ce90d6df361e785
Erty Seidel <ertysdl+github@gmail.com>
2013-08-31 16:53:55 -0600

    Added mouseup and mousedown listeners
    Had some problems getting the "this" in the mouse listeners to be the
    iagame object. Finally created closures for the iagame element which
    would bind correctly.

--- canvas.js
+++ canvas.js
@@ -2,6 +2,7 @@
 	var IAGame = function (canvasElement, images){
 		if(!canvasElement.getContext) throw "canvasElement could not get context";
 		var ctx = canvasElement.getContext("2d"); //get canvas context element
+
 		this.imagesLoaded = 0; //number of images which have finished loading
 
 		this.images = {
@@ -27,15 +28,21 @@
 			new Dot("green", {x: 250, y: 0}),
 			new Dot("black", {x: 375, y: 0}),
 			new Dot("black", {x: 500, y: 0})
-		]
+		];
+
+		this.currentDot = null;
 
 		/*
          * Update function which runs to update positions and check for winning
 		 */
 		this.update = function(){
 			if(this.imagesLoaded < Object.keys(this.images).length) return;
+			if(this.currentDot){
+				this.currentDot.pos.x = mouseX;
+				this.currentDot.pos.y = mouseY;
+			}
+		};
 
-		}
 		/*
 		 * Draw function which repaints canvas at (hopefully) 60 frames per second
 		 */
@@ -45,15 +52,34 @@
 			this.dots.forEach(function(dot){
 				ctx.drawImage(this.images[dot.color], dot.pos.x, dot.pos.y);
 			}.bind(this));
-		}
+		};
+
+		this.mousedown = function(evt){
+			this.dots.forEach(function(dot){
+				if(dot.contains({ x: evt.x, y: evt.y })){
+					this.currentDot = dot;
+				}
+			}.bind(this));
+		};
+
+		this.mouseup = function(evt){
+			this.currentDot = null;
+		};
 
 	};
 
 	var Dot = function(color, start){
 		this.color = color;
 		this.start = start;
+		this.size = {x: 100, y: 100},
 		this.pos = {x: start.x, y: start.y};
 	}
+
+	Dot.prototype.contains = function(pos){
+		var xSquared = Math.pow(pos.x - (this.pos.x + (this.size.x / 2)), 2);
+		var ySquared = Math.pow(pos.y - (this.pos.y + (this.size.y / 2)), 2);
+		return ((xSquared + ySquared) < Math.pow((this.size.x / 2), 2))
+	};
 
 	exports.IAGame = IAGame;
 })(window);
@@ -65,6 +91,9 @@ window.onload = function(){
 	canvas.width = "600";
 	canvas.height = "700";
 	iagame = new IAGame(canvas);
+
+	canvas.addEventListener("mousedown", function(evt){iagame.mousedown(evt)});
+	canvas.addEventListener("mouseup", function(evt){iagame.mouseup(evt)});
 
 	var updateGame = function(){
 		window.requestAnimationFrame(updateGame);
