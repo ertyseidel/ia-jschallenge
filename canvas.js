63da1a871230f95ee1436ccf0e3e10612233a81b
Erty Seidel <ertysdl+github@gmail.com>
2013-09-08 20:55:15 -0600

    Added comments, minor bugfixes
    Commented Code is Good Code

--- canvas.js
+++ canvas.js
@@ -4,9 +4,9 @@
 		this.width = canvasElement.width;
 		this.height = canvasElement.height;
 
-		this.backgroundCache = null;
+		this.backgroundCache = null; //a canvas element for drawing the background quickly
 
-		this.dirty = true;
+		this.dirty = true; //set to true if we need to update the canvas
 
 		this.images = {
 			background: "ia-logo-back.png",
@@ -14,7 +14,7 @@
 			blue: "ia-logo-dot-blue.png",
 			green: "ia-logo-dot-green.png",
 			red: "ia-logo-dot-red.png"
-		}
+		};
 
 		this.dots = [
 			new Dot("blue", {x: 0, y: 0}),
@@ -32,11 +32,12 @@
 			new Drop("black", {x: 419, y: 388})
 		];
 
-		this.currentDot = null;
+		this.currentDot = null; //the dot that the user is holding
 		this.currentMousePos = {x: 0, y: 0};
 
 		this.won = false;
 
+		//Load each of the images, and keep track of how many we've loaded
 		for (var key in this.images){
 			var src = this.images[key];
 			this.images[key] = new Image();
@@ -51,12 +52,13 @@
 		 */
 		this.update = function(){
 			var dotsCorrect = 0;
-			if(this.imagesLoaded < Object.keys(this.images).length) return;
+			if(this.imagesLoaded < Object.keys(this.images).length) return; //break until we have all of the images loaded
 			if(this.currentDot){
 				this.dirty = true;
+
 				this.currentDot.pos.x = this.currentMousePos.x - this.currentDot.mousePos.x;
 				this.currentDot.pos.y = this.currentMousePos.y - this.currentDot.mousePos.y;
-
+				//loop through the drops and check for a collision
 				var correctDrop = false;
 				this.drops.forEach(function(drop){
 					if(drop.contains(this.currentDot.getCenter())){
@@ -75,12 +77,13 @@
 				this.currentDot.correctlyDropped = correctDrop;
 			}
 
+			//loop through the dots and update them
 			this.dots.forEach(function(dot){
 				if(!dot.correctlyDropped && dot !== this.currentDot){
 					if(dot.pos.x != dot.start.x){
 						dot.pos.x -= (dot.pos.x - dot.start.x) / 10;
 						if(Math.abs(dot.pos.x - dot.start.x) < .1) dot.pos.x = dot.start.x;
-						this.dirty = true;	
+						this.dirty = true;
 					}
 					if(dot.pos.y != dot.start.y){
 						dot.pos.y -= (dot.pos.y - dot.start.y) / 10;
@@ -94,22 +97,23 @@
 
 			var tempWon = this.won;
 			this.won = dotsCorrect == Object.keys(this.images).length;
-			if(this.won != tempWon) this.dirty = true;
+			if(this.won != tempWon) this.dirty = true; //checks if we have changed win state since last update
 		};
 
 		/*
 		 * Draw function which repaints canvas at (hopefully) 60 frames per second
 		 */
 		this.draw = function(ctx){
-			if(!this.dirty) return;
-			console.log(this.won);
+			if(!this.dirty) return; //don't draw unless there's something to draw
+			if(this.imagesLoaded < Object.keys(this.images).length) return; //don't draw unless all the images are loaded
 			ctx.clearRect(0, 0, this.width, this.height);
 
-			if(this.backgroundCache == null){
+			if(this.backgroundCache == null){ //cache our background image in a canvas which should be faster to draw
 				var that = this;
 				this.backgroundCache = renderToCanvas(ctx.canvas.width, ctx.canvas.height - 100, function(ctx){
-					if(that.imagesLoaded < Object.keys(that.images).length) return;
+					//background
 					ctx.drawImage(that.images.background, 0, 0);
+					//reset button
 					ctx.fillStyle = "#c52d47";
 					ctx.fillRect(ctx.canvas.width - 100, ctx.canvas.height - 20, 100, 20);
 					ctx.fillStyle = "#000000";
@@ -133,6 +137,7 @@
 			this.dirty = false;
 		};
 
+		//Resets the board
 		this.reset = function(){
 			this.dots.forEach(function(dot){
 				dot.pos.x = dot.start.x;
@@ -148,6 +153,7 @@
 			this.dirty = true;
 		}
 
+		//Called every time the mouse moves
 		this.mousemove = function(evt){
 			if(this.currentDot){
 				this.currentMousePos.x = evt.offsetX;
@@ -155,6 +161,7 @@
 			}
 		}
 
+		//Called every time the mouse button is pressed
 		this.mousedown = function(evt){
 			this.currentMousePos.x = evt.offsetX;
 			this.currentMousePos.y = evt.offsetY;
@@ -171,18 +178,23 @@
 			}
 		};
 
+		//Called every time the mouse button is released
 		this.mouseup = function(evt){
 			this.currentDot = null;
 		};
 
 	};
 
+	//Helper function for both Dots and Drops
+	//Returns if the current object has point pos inside it
 	var contains = function(pos){
 		var xSquared = Math.pow(pos.x - (this.pos.x + (this.size.x / 2)), 2);
 		var ySquared = Math.pow(pos.y - (this.pos.y + (this.size.y / 2)), 2);
 		return ((xSquared + ySquared) < Math.pow((this.size.x / 2), 2))
 	};
 
+	//Helper function for both Dots and Drops
+	//Returns the center of the current object
 	var getCenter = function(){
 		return {x: this.pos.x + this.size.x / 2, y: this.pos.y + this.size.y / 2};
 	}
@@ -195,9 +207,7 @@
 		this.mousePos = {x: 0, y: 0}
 		this.correctlyDropped = false;
 	}
-
 	Dot.prototype.contains = contains;
-
 	Dot.prototype.getCenter = getCenter;
 
 	var Drop = function(color, pos){
@@ -206,9 +216,7 @@
 		this.size = {x: 100, y: 100};
 		this.occupied = null;
 	};
-
 	Drop.prototype.contains = contains;
-
 	Drop.prototype.getCenter = getCenter;
 
 	//from http://kaioa.com/node/103
@@ -227,11 +235,12 @@ window.onload = function(){
 	var canvas = document.getElementById("app");
 	canvas.width = "600";
 	canvas.height = "700";
-	canvas.style.position = "relative";
+	canvas.style.position = "relative"; //req'd for the offsetX and offsetY
 	var iagame = new IAGame(canvas);
 	if(!canvas.getContext) throw "canvas could not get context";
 	var ctx = canvas.getContext("2d"); //get canvas context element
 
+	//Bind mouse listeners to the canvas element
 	canvas.addEventListener("mousedown", function(evt){iagame.mousedown(evt)});
 	canvas.addEventListener("mouseup", function(evt){iagame.mouseup(evt)});
 	canvas.addEventListener("mousemove", function(evt){iagame.mousemove(evt)});
